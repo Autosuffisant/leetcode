54618651514cbbbf63fd6f2cec24860b27c7c5f5
Gabriel Huguenin-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2024-01-13 22:36:11 +0100

    Slight optimization of 1-13

--- 1-january/1-13-1347.py
+++ 1-january/1-13-1347.py
@@ -4,23 +4,17 @@ class Solution:
         tLetters = dict()
 
         for letter in s:
-            if not sLetters.get(letter):
-                sLetters[letter] = 0
-            sLetters[letter] = sLetters[letter] + 1
+            sLetters[letter] = sLetters.get(letter, 0) + 1
 
         for letter in t:
-            if not tLetters.get(letter):
-                tLetters[letter] = 0
-            tLetters[letter] = tLetters[letter] + 1
+            tLetters[letter] = tLetters.get(letter, 0) + 1
 
         for letter in "abcdefghijklmnopqrstuvwxyz":
             sOccurence = sLetters.get(letter) or 0
             tOccurence = tLetters.get(letter) or 0
             if sOccurence > tOccurence:
-                # Change the letter in the string to match the same occurence
-                s = s.replace(letter, "", sOccurence - tOccurence)
-            elif tOccurence > sOccurence:
-                # Add the letter to the string to match the same occurence
-                s = s.replace(letter, letter * (tOccurence - sOccurence))
+                sLetters[letter] = sOccurence - tOccurence
+            else:
+                sLetters[letter] = 0
 
-        return 0
+        return sum(sLetters.values())
